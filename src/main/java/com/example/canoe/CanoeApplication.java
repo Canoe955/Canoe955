1f7f3f16da879b537b3afa4a6ddc27f0985a18a4
canoe <canoe955yan@gmail.com>
2021-07-29 14:38:08 +0800

    canoe

---
+++ src/main/java/com/example/canoe/CanoeApplication.java
@@ -0,0 +1,13 @@
+package com.example.canoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanoeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/canoe/CanoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanoeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
